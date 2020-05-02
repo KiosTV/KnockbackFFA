e7f2d45574987692230b6bde9d0c90b7eee2c9d7
Andreas Butzmuehlen <andreas.butzmuehlen@outlook.de>
2020-05-02 13:01:01 +0200

    Initial commit

---
+++ src/com/kiostv/kbffa/Main.java
@@ -0,0 +1,18 @@
+package com.kiostv.kbffa;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        //TODO Plugin Start-logic
+    }
+
+    @Override
+    public void onDisable(){
+        //TODO Plugin Stop-logic
+    }
+
+}
